b2aa8a62cb38cc8fba9a03e167c8a312b610880a
Pablo Strasser <strasserpablo@bluewin.ch>
2025-01-30 15:57:39 +0100

    Update chart number

--- build/FuelConsumption.py
+++ build/FuelConsumption.py
@@ -179,7 +179,6 @@ def create_app():
                   response = make_response({
                               "msg": "Started the process"
                               })
-
             except Exception as e:
                   logger_workflow.error('Got exception '+str(e)+'\n'+traceback.format_exc()+'\n'+'So we are ignoring the message', extra={'status': 'CRITICAL'})
                   # HTTP answer that the message is malformed. This message will then be discarded only the fact that a sucess return code is returned is important.
